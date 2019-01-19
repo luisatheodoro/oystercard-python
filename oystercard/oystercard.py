5e605e65aca62231be57cfc6b0c75e2e82e19a12
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-19 20:36:21 +0000

    adding a function to check if max limit was exceeded

--- oystercard/oystercard.py
+++ oystercard/oystercard.py
@@ -5,8 +5,11 @@ class OysterCard:
         self.MAX_LIMIT = 90
 
     def top_up(self, value):
-        if self.balance + value > self.MAX_LIMIT:
+        if self._is_max_limit_exceeded:
             raise Exception("Your balance is currently {} and your limit is {}".format(self.balance,
                                                                                        self.MAX_LIMIT))
         self.balance += value
         return self.balance
+
+    def _is_max_limit_exceeded(self, value):
+        return self.balance + value > self.MAX_LIMIT
