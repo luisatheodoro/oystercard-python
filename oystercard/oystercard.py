027d65a9b6aa249599be827b21525d0b991d0b6a
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-19 20:22:00 +0000

    raises error if top above card limit

--- oystercard/oystercard.py
+++ oystercard/oystercard.py
@@ -1,8 +1,12 @@
 class OysterCard:
+
     def __init__(self, balance=0):
         self.balance = balance
+        self.MAX_LIMIT = 90
 
     def top_up(self, value):
+        if self.balance + value > self.MAX_LIMIT:
+            raise Exception("Your balance is currently {} and your limit is {}".format(self.balance,
+                                                                                       self.MAX_LIMIT))
         self.balance += value
         return self.balance
-
--- test/test_oystercard.py
+++ test/test_oystercard.py
@@ -10,6 +10,15 @@ class OysterCardTest(unittest.TestCase):
     def test_customer_can_top_up(self):
         self.assertEqual(OysterCard().top_up(10), 10)
 
+    def test_raises_error_if_top_up_above_card_limit(self):
+        with self.assertRaises(Exception) as cm:
+            OysterCard().top_up(91)
+        self.assertEqual(
+            "Your balance is currently {} and your limit is {}".format(OysterCard().balance,
+                                                                       OysterCard().MAX_LIMIT),
+            str(cm.exception)
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
