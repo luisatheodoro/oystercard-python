81192ede3985cec71a6a2542a2e1cc53aad8e5bc
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-19 18:34:09 +0000

    user can top up

--- oystercard/oystercard.py
+++ oystercard/oystercard.py
@@ -1,3 +1,8 @@
 class OysterCard:
     def __init__(self, balance=0):
         self.balance = balance
+
+    def top_up(self, value):
+        self.balance += value
+        return self.balance
+
--- test/test_oystercard.py
+++ test/test_oystercard.py
@@ -7,6 +7,9 @@ class OysterCardTest(unittest.TestCase):
     def test_oyster_card_starts_with_zero_balance(self):
         self.assertEqual(OysterCard().balance, 0)
 
+    def test_customer_can_top_up(self):
+        self.assertEqual(OysterCard().top_up(10), 10)
+
 
 if __name__ == "__main__":
     unittest.main()
