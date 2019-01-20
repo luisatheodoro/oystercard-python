bc81bfdd6f03d7e58a14dbaaaf98d4616c492bee
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-20 00:26:00 +0000

    oystercard can store entry station

--- oystercard/oystercard.py
+++ oystercard/oystercard.py
@@ -5,6 +5,7 @@ class OysterCard:
     def __init__(self):
         self.balance = 0
         self.is_in_journey = False
+        self.entry_station = None
 
     def top_up(self, value):
         if self._is_max_limit_exceeded(value):
@@ -13,15 +14,18 @@ class OysterCard:
         self.balance += value
         return self.balance
 
-    def touch_in(self):
+    def touch_in(self, entry_station):
         if self._is_not_enough_funds():
             raise Exception("Your don't have enough funds")
         else:
             self.is_in_journey = True
+            self.entry_station = entry_station
+            return self.balance
 
     def touch_out(self):
         self._deduct(self.MIN_FARE)
         self.is_in_journey = False
+        self.entry_station = None
         return self.balance
 
     def _deduct(self, value):
--- test/test_oystercard.py
+++ test/test_oystercard.py
@@ -1,8 +1,13 @@
 import unittest
+import mock
 from oystercard.oystercard import OysterCard
 
 
 class OysterCardTest(unittest.TestCase):
+    def victoria_station(self):
+        m = mock.Mock()
+        m.victoria_station = 'victoria station'
+        return m.victoria_station
 
     def test_oyster_card_starts_with_zero_balance(self):
         self.assertEqual(OysterCard().balance, 0)
@@ -31,19 +36,19 @@ class OysterCardTest(unittest.TestCase):
     def test_is_in_journey_after_touch_in(self):
         oyster_card = OysterCard()
         oyster_card.top_up(10)
-        oyster_card.touch_in()
+        oyster_card.touch_in(self.victoria_station())
         self.assertIs(oyster_card.is_in_journey, True)
 
     def test_is_not_in_journey_after_touch_out(self):
         oyster_card = OysterCard()
         oyster_card.top_up(10)
-        oyster_card.touch_in()
+        oyster_card.touch_in(self.victoria_station())
         oyster_card.touch_out()
         self.assertIs(oyster_card.is_in_journey, False)
 
     def test_raises_error_if_touch_in_without_credit(self):
         with self.assertRaises(Exception) as cm:
-            OysterCard().touch_in()
+            OysterCard().touch_in(self.victoria_station())
         self.assertEqual(
             "Your don't have enough funds",
             str(cm.exception)
@@ -53,11 +58,17 @@ class OysterCardTest(unittest.TestCase):
         with self.assertRaises(Exception) as cm:
             oyster_card = OysterCard()
             oyster_card.top_up(2)
-            oyster_card.touch_in()
+            oyster_card.touch_in(self.victoria_station())
         self.assertEqual(
             "Your don't have enough funds",
             str(cm.exception)
         )
+
+    def test_card_store_entry_station(self):
+        oyster_card = OysterCard()
+        oyster_card.top_up(10)
+        oyster_card.touch_in(self.victoria_station())
+        self.assertEqual(oyster_card.entry_station, self.victoria_station())
 
 
 if __name__ == "__main__":
